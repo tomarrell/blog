ec89f195da783375cfc6e1c14ed3c5de31230bd5
Tom Arrell <tom.arrell@gmail.com>
2020-06-11 18:25:49 +0200

    Try new local address

--- src/main.rs
+++ src/main.rs
@@ -86,7 +86,7 @@ fn file(path: PathBuf) -> impl Fn(&HttpRequest<AppState>) -> Result<fs::NamedFil
 fn main() {
     pretty_env_logger::init_timed();
 
-    let address = "127.0.0.1:8080";
+    let address = "0.0.0.0:8080";
 
     info!("Starting server on address {}", address);
 
